78fc3b470457312fad5038f52976bddd174c8c3e
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-09-24 14:08:38 +0500

    fix

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request, HTTPException
 from db import update_or_create_config, get_config_from_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 
 @app.get("/")
